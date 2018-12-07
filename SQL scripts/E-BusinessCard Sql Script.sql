3a214a0ea773764cd5571d6e63f1d7bd99603ddf
nosrat1004 <38430922+nosrat1004@users.noreply.github.com>
2018-12-07 23:11:42 +0600

    Sql Script added

---
+++ SQL scripts/E-BusinessCard Sql Script.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE EBDB
+
+USE EBDB
+
+CREATE TABLE [USER]
+(
+	UserID				INT PRIMARY KEY IDENTITY,
+	UserName			VARCHAR(50)  NOT NULL,
+	FullName			VARCHAR(50)  NOT NULL,
+	[Password]			VARCHAR(50)	 NOT NULL,
+	Email				VARCHAR(200) NOT NULL,
+	PhoneNumber			VARCHAR(15)  NOT NULL,
+	[Address]			VARCHAR(MAX) NOT NULL,
+	DOB					DATETIME	 NOT NULL
+)
+
+CREATE PROC SELECT_USER_BY_USERNAME_N_PASSWORD
+@username	VARCHAR(50),
+@password	VARCHAR(50)
+AS
+BEGIN
+	SELECT
+		[UserID]
+	FROM 
+		[User]
+	WHERE
+		Username = @username AND [Password] = @password		
+END
+
+CREATE PROC ADD_NEW_USER
+@username		VARCHAR(50),
+@fullname		VARCHAR(50),
+@password		VARCHAR(50),
+@email			VARCHAR(200),
+@address		VARCHAR(MAX),
+@dob			DATETIME,
+@phonenumber	VARCHAR(15)
+AS
+BEGIN
+	INSERT INTO 
+		[User]	
+	VALUES
+		(@username, @fullname, @password, @email, @phoneNumber, @address, @DOB)
+END
+
+
+SELECT * FROM [User]
